4eb41901f2d70d2e7ce27147992f845f072a1134
mailengm <mailen.gomezmayol@gmail.com>
2021-06-12 11:58:27 -0300

    get_users_ID. Minor corrections in create_url function. Now saves IDs to file

--- get_users_ID.py
+++ get_users_ID.py
@@ -19,23 +19,21 @@ def create_url(usernames_list):
     Create the URL to get the usernames ID
     """
     # Specify the usernames that you want to lookup below
-    separator = ", "
-    usernames = f"usernames= {separator.join(usernames_list)}"
+    separator = ","
+    usernames = f"usernames={separator.join(usernames_list).casefold()}"
     user_fields = "user.fields=id"
-    # User fields are adjustable, options include:
-    # created_at, description, entities, id, location, name,
-    # pinned_tweet_id, profile_image_url, protected,
-    # public_metrics, url, username, verified, and withheld
+
     url = "https://api.twitter.com/2/users/by?{}&{}".format(usernames, user_fields)
     return url
 
-
 def create_headers(bearer_token):
+    """ Create header using the bearer token """
     headers = {"Authorization": "Bearer {}".format(bearer_token)}
     return headers
 
 
 def connect_to_endpoint(url, headers):
+    """ Connect to endpoint and process the request """
     response = requests.request("GET", url, headers=headers)
     print(response.status_code)
     if response.status_code != 200:
@@ -49,10 +47,17 @@ def connect_to_endpoint(url, headers):
 
 def main():
     bearer_token = auth()
-    url = create_url()
+
+    username_list = ["Ambitocom", "C5N", "clarincom", "cronica", "infobae", 
+    "_IPNoticias", "LANACION", "lanacionmas", "pagina12", "todonoticias"]
+
+    url = create_url(username_list)
     headers = create_headers(bearer_token)
     json_response = connect_to_endpoint(url, headers)
     print(json.dumps(json_response, indent=4, sort_keys=True))
+
+    with open('user_ID.txt', 'w') as outfile:
+        json.dump(data, outfile)
 
 
 if __name__ == "__main__":
